c2cab9b480a2959f88bda4e5313ea1cc70526c77
Nabeel Babar <iamnabeel528@gmail.com>
2025-08-30 19:17:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
-from router import upload_router
-from router import retrieve_router
+from router.upload_router import router as upload_router
+from router.retrieve import router as retrieve_router
+import os
 
 app = FastAPI(title="PDF Upload and Process API")
 
-app.include_router(upload_router.router, prefix="/files")
-app.include_router(retrieve_router.router, prefix="/retrieve")
-
+app.include_router(upload_router, prefix="/files")
+app.include_router(retrieve_router, prefix="/retrieve")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=4545, reload=True)
+    port = int(os.getenv("PORT", 4545))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
